03497fcf52e21263812d7f2a141977d75bba3397
Bruno <brunocapello12@gmail.com>
2026-06-09 02:42:45 -0300

    frmAlumno en punga, falta el boton editar, eliminar y mostrar la matriz

--- pryCapelloAcademia/frmAlumno.Designer.cs
+++ pryCapelloAcademia/frmAlumno.Designer.cs
@@ -28,12 +28,308 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panGeneral = new System.Windows.Forms.Panel();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.lblContacto = new System.Windows.Forms.Label();
+            this.txtContacto = new System.Windows.Forms.MaskedTextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.lblFechaRegistro = new System.Windows.Forms.Label();
+            this.lblFechaActualizacion = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colDni = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colApellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDireccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colContacto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFechaNacimiento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.panGeneral.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panGeneral
+            // 
+            this.panGeneral.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panGeneral.Controls.Add(this.btnEliminar);
+            this.panGeneral.Controls.Add(this.dataGridView1);
+            this.panGeneral.Controls.Add(this.btnEditar);
+            this.panGeneral.Controls.Add(this.btnAgregar);
+            this.panGeneral.Controls.Add(this.lblFechaActualizacion);
+            this.panGeneral.Controls.Add(this.lblFechaRegistro);
+            this.panGeneral.Controls.Add(this.dtpFechaNacimiento);
+            this.panGeneral.Controls.Add(this.lblFecha);
+            this.panGeneral.Controls.Add(this.txtContacto);
+            this.panGeneral.Controls.Add(this.lblContacto);
+            this.panGeneral.Controls.Add(this.lblDireccion);
+            this.panGeneral.Controls.Add(this.txtDireccion);
+            this.panGeneral.Controls.Add(this.lblApellido);
+            this.panGeneral.Controls.Add(this.txtApellido);
+            this.panGeneral.Controls.Add(this.lblNombre);
+            this.panGeneral.Controls.Add(this.txtNombre);
+            this.panGeneral.Controls.Add(this.lblDni);
+            this.panGeneral.Controls.Add(this.txtDni);
+            this.panGeneral.Location = new System.Drawing.Point(12, 12);
+            this.panGeneral.Name = "panGeneral";
+            this.panGeneral.Size = new System.Drawing.Size(1113, 338);
+            this.panGeneral.TabIndex = 0;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(96, 13);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(178, 22);
+            this.txtDni.TabIndex = 0;
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(23, 16);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(30, 16);
+            this.lblDni.TabIndex = 1;
+            this.lblDni.Text = "DNI";
+            this.lblDni.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(96, 48);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(178, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(23, 51);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(23, 86);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(57, 16);
+            this.lblApellido.TabIndex = 5;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(96, 83);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(178, 22);
+            this.txtApellido.TabIndex = 2;
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(23, 121);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(64, 16);
+            this.lblDireccion.TabIndex = 7;
+            this.lblDireccion.Text = "Dirección";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(96, 118);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(178, 22);
+            this.txtDireccion.TabIndex = 3;
+            // 
+            // lblContacto
+            // 
+            this.lblContacto.AutoSize = true;
+            this.lblContacto.Location = new System.Drawing.Point(23, 156);
+            this.lblContacto.Name = "lblContacto";
+            this.lblContacto.Size = new System.Drawing.Size(60, 16);
+            this.lblContacto.TabIndex = 9;
+            this.lblContacto.Text = "Contacto";
+            // 
+            // txtContacto
+            // 
+            this.txtContacto.Location = new System.Drawing.Point(96, 153);
+            this.txtContacto.Mask = "(999)000-0000";
+            this.txtContacto.Name = "txtContacto";
+            this.txtContacto.Size = new System.Drawing.Size(178, 22);
+            this.txtContacto.TabIndex = 4;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(23, 191);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(132, 16);
+            this.lblFecha.TabIndex = 11;
+            this.lblFecha.Text = "Fecha de nacimiento";
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.CustomFormat = "09/06/2026";
+            this.dtpFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(161, 188);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(113, 22);
+            this.dtpFechaNacimiento.TabIndex = 5;
+            // 
+            // lblFechaRegistro
+            // 
+            this.lblFechaRegistro.AutoSize = true;
+            this.lblFechaRegistro.Location = new System.Drawing.Point(23, 226);
+            this.lblFechaRegistro.Name = "lblFechaRegistro";
+            this.lblFechaRegistro.Size = new System.Drawing.Size(112, 16);
+            this.lblFechaRegistro.TabIndex = 13;
+            this.lblFechaRegistro.Text = "Fecha de registro";
+            // 
+            // lblFechaActualizacion
+            // 
+            this.lblFechaActualizacion.AutoSize = true;
+            this.lblFechaActualizacion.Location = new System.Drawing.Point(23, 261);
+            this.lblFechaActualizacion.Name = "lblFechaActualizacion";
+            this.lblFechaActualizacion.Size = new System.Drawing.Size(145, 16);
+            this.lblFechaActualizacion.TabIndex = 14;
+            this.lblFechaActualizacion.Text = "Fecha de actualización";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(26, 294);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 27);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(112, 294);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 27);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colDni,
+            this.colNombre,
+            this.colApellido,
+            this.colDireccion,
+            this.colContacto,
+            this.colFechaNacimiento});
+            this.dataGridView1.Location = new System.Drawing.Point(295, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(804, 308);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // colDni
+            // 
+            this.colDni.HeaderText = "DNI";
+            this.colDni.MinimumWidth = 6;
+            this.colDni.Name = "colDni";
+            this.colDni.Width = 125;
+            // 
+            // colNombre
+            // 
+            this.colNombre.HeaderText = "Nombre";
+            this.colNombre.MinimumWidth = 6;
+            this.colNombre.Name = "colNombre";
+            this.colNombre.Width = 125;
+            // 
+            // colApellido
+            // 
+            this.colApellido.HeaderText = "Apellido";
+            this.colApellido.MinimumWidth = 6;
+            this.colApellido.Name = "colApellido";
+            this.colApellido.Width = 125;
+            // 
+            // colDireccion
+            // 
+            this.colDireccion.HeaderText = "Dirección";
+            this.colDireccion.MinimumWidth = 6;
+            this.colDireccion.Name = "colDireccion";
+            this.colDireccion.Width = 125;
+            // 
+            // colContacto
+            // 
+            this.colContacto.HeaderText = "Contacto";
+            this.colContacto.MinimumWidth = 6;
+            this.colContacto.Name = "colContacto";
+            this.colContacto.Width = 125;
+            // 
+            // colFechaNacimiento
+            // 
+            this.colFechaNacimiento.HeaderText = "Fecha de nacimiento";
+            this.colFechaNacimiento.MinimumWidth = 6;
+            this.colFechaNacimiento.Name = "colFechaNacimiento";
+            this.colFechaNacimiento.Width = 125;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(199, 294);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 27);
+            this.btnEliminar.TabIndex = 16;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // frmAlumno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAlumno";
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1136, 361);
+            this.Controls.Add(this.panGeneral);
+            this.Name = "frmAlumno";
+            this.Text = "Registrar Alumno";
+            this.panGeneral.ResumeLayout(false);
+            this.panGeneral.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panGeneral;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.MaskedTextBox txtContacto;
+        private System.Windows.Forms.Label lblContacto;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.Label lblFechaRegistro;
+        private System.Windows.Forms.Label lblFechaActualizacion;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDni;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colApellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDireccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colContacto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFechaNacimiento;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
--- pryCapelloAcademia/frmAlumno.cs
+++ pryCapelloAcademia/frmAlumno.cs
@@ -12,9 +12,70 @@ namespace pryCapelloAcademia
 {
     public partial class frmAlumno : Form
     {
+
+        int indiceFila = 0;
+        string[,] arrAlumno = new string[2, 6];
+
         public frmAlumno()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < arrAlumno.GetLength(0); i++)
+            {
+                if (txtDni.Text == "")
+                {
+                    MessageBox.Show("Ingrese un DNI");
+                    txtDni.Focus();
+                    return;
+                }
+                if (txtNombre.Text != "")
+                {
+                    MessageBox.Show("Ingrese un nombre");
+                    txtNombre.Focus();
+                    return;
+                }
+                if (txtApellido.Text == "")
+                {
+                    MessageBox.Show("Ingrese un apellido");
+                    txtApellido.Focus();
+                    return;
+                }
+                if (txtDireccion.Text == "")
+                {
+                    MessageBox.Show("Ingrese una dirección");
+                    txtDireccion.Focus();
+                    return;
+                }
+                if (txtContacto.Text.ToString() == "")
+                {
+                    MessageBox.Show("Ingrese un teléfono");
+                    txtContacto.Focus();
+                    return;
+                }
+                if (dtpFechaNacimiento.Value == null)
+                {
+                    MessageBox.Show("Ingrese una fecha de nacimiento");
+                    dtpFechaNacimiento.Focus();
+                    return;
+                }
+
+                arrAlumno[indiceFila, 0] = txtDni.Text;
+                arrAlumno[indiceFila, 1] = txtNombre.Text;
+                arrAlumno[indiceFila, 2] = txtApellido.Text;
+                arrAlumno[indiceFila, 3] = txtDireccion.Text;
+                arrAlumno[indiceFila, 4] = txtContacto.Text;
+                arrAlumno[indiceFila, 5] = dtpFechaNacimiento.Value.ToString("dd/MM/yyyy");
+
+                indiceFila++;
+            }
         }
     }
 }
